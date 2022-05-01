f0e42227b7f8d8b0dd6b8b046b0aafea7350556b
Mrteller <39244601+Mrteller@users.noreply.github.com>
2022-05-01 20:14:21 +0300

    Added readme.

--- PetSearch/Core/data/api/APIConstants.swift
+++ PetSearch/Core/data/api/APIConstants.swift
@@ -1,6 +1,6 @@
 enum APIConstants {
   static let host = "api.petfinder.com"
   static let grantType = "client_credentials"
-  static let clientId = "YourKeyHere"
-  static let clientSecret = "YourSecretHere"
+  static let clientId = "iy8zRbPzcSDuIPZGOUy8yYdPm8goxW5lnb8DmVQIJOs0IbRjbR"
+  static let clientSecret = "5OTIld6PyWwmZOSwqDV3sg6DdnC4lf2JdahlLwsl"
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+#  PetSearch
+## The purpose
+* To organize the project with a layered architecture using **feature grouping**
+* To ensure code has **high cohesion** and **low coupling**
+* To follow **SOLID** principles and **design patterns**
+* To introduce the **domain layer**
+
